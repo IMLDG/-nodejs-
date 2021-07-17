ca9f815595b824bebecd8e8af15432c8e4847729
LDG <wotn2517@naver.com>
2021-07-18 06:00:19 +0900

    front를 위한 js

--- app/main.js
+++ app/main.js
@@ -8,6 +8,8 @@ const app=express();
 app.set("views","./src/views");
 app.set("view engine", "ejs");//화면 engine을 ejs로 설정
 
+app.use(express.static(`${__dirname}/src/public`));//express.static=>image,css,js와 같은 정적 파일을 제공해줌, ${__dirname}=>현재 디렉터리 이름
+
 module.exports=app;
 
 //node.js에서의 index.js, app.js, server.js 차이
