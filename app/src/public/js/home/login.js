87e5af4fa546a2a0c54af2d4eeec48895fa2d099
LDG <wotn2517@naver.com>
2021-07-18 06:48:05 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
+//문서에서 값(아이디,비밀번호 등)을 받아오기
+//DOM(Document Object Model)사용하기
+
 "use strict";
 
-console.log("Hello");
+const id = document.querySelector("#id");//# => tag에 id로 분류되어 있는 것을 가져옴
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login);//Event의 종류 => https://m.blog.naver.com/PostView.nhn?isHttpsRedirect=true&blogId=qbxlvnf11&logNo=220877806711&categoryNo=24&proxyReferer=
+
+function login(){
+    const req = {
+        id : id.value,
+        password : password.value
+    }
+};
